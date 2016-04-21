ad5302563984eceb84f54e187a4485009e906109
albertogarrido <zgz.alberto@gmail.com>
2016-04-21 13:01:15 +0200

    Add class Application, generate basic packages, move activity to UI

---
+++ app/src/main/java/net/albertogarrido/classifieds/ClassifiedsApplication.java
@@ -0,0 +1,9 @@
+package net.albertogarrido.classifieds;
+
+import android.app.Application;
+
+/**
+ * Created by AlbertoGarrido on 4/21/16.
+ */
+public class ClassifiedsApplication extends Application {
+}
--- app/src/main/java/net/albertogarrido/classifieds/ui/MainActivity.java
+++ app/src/main/java/net/albertogarrido/classifieds/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package net.albertogarrido.classifieds;
+package net.albertogarrido.classifieds.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.albertogarrido.classifieds.R;
 
 public class MainActivity extends AppCompatActivity {
 
