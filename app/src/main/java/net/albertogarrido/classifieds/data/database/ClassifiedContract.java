f37d2727b76fc325ff619702b355034082077d38
albertogarrido <zgz.alberto@gmail.com>
2016-04-21 16:01:11 +0200

    remove quotes table

--- app/src/main/java/net/albertogarrido/classifieds/data/database/ClassifiedContract.java
+++ app/src/main/java/net/albertogarrido/classifieds/data/database/ClassifiedContract.java
@@ -21,11 +21,4 @@ public class ClassifiedContract {
         public static final String COLUMN_NAME_IMAGE_LINK = "image_link";
         public static final String COLUMN_NAME_VISITED_TIMES = "visited_times";
     }
-
-    public static abstract class QuotesEntry implements BaseColumns {
-        public static final String TABLE_NAME = "quotes";
-        public static final String COLUMN_NAME_QUOTE = "quote";
-        public static final String COLUMN_NAME_AUTHOR = "author";
-        public static final String COLUMN_NAME_DATE = "date";
-    }
 }
